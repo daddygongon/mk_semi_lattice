bfc3cf9e8022485b5583a6d8f97fb9ecb7d10e14
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2025-10-20 14:14:28 +0900

    first commit

---
+++ lib/mk_semi_lattice.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mk_semi_lattice/version"
+
+module MkSemiLattice
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "mk_semi_lattice"
+
+require "minitest/autorun"
