fac4389819981df671e86a1e637b6e340290e1b5
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2025-12-25 14:23:54 +0900

    hc trial

--- lib/hc.rb
+++ lib/hc.rb
@@ -1,10 +1,17 @@
 require 'thor'
+require_relative "mk_semi_lattice/version"
+require_relative "mk_stack/mk_stack"
 
 class CLI < Thor
   desc "view", "View Semi Lattice Graph"
   def view(*argv)
     system "mk_semi_lattice", *argv
   end
-end
 
-CLI.start.run(ARGV)
+  desc 'stack', 'make stacks'
+  def stack(*argv)
+      MkStack.new(argv).run
+  end
+end 
+
+CLI.start(ARGV)
