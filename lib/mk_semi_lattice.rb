7be44b6a345db2784c57aa663bde114ac14d20c8
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2025-12-25 10:50:46 +0900

    hoge

--- lib/mk_semi_lattice.rb
+++ lib/mk_semi_lattice.rb
@@ -7,13 +7,8 @@ require 'rbconfig'
 require 'fileutils'
 
 require_relative "mk_semi_lattice/version"
-require_relative "mk_semi_lattice/ruby2d_action"
 require_relative "mk_semi_lattice/ruby2d_run"
-require_relative "mk_semi_lattice/init_env"
-require_relative "mk_semi_lattice/option_manager"
-require_relative "mk_semi_lattice/manage_yaml/mk_node_edge_yaml"
-require_relative "mk_semi_lattice/manage_yaml/mk_semi_lattice_yaml"
-require_relative "mk_semi_lattice/sl_components"
+
 
 class Error < StandardError; end
 
